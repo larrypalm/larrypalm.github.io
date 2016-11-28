66f831ad6718c36da2492341abde1b85a062c997
larrypalm <palm.gustav@gmail.com>
2016-11-28 23:44:02 +0100

    alot of changes done

--- js/main.js
+++ js/main.js
@@ -5,17 +5,59 @@ $(document).ready(function(){
 	});
 
 
-	$("#hide").hide();
+	//cascade CV	
+	$(".show").click(function(){
 
-
-
-	$("#show").click(function(){
-		$("#hide").slideToggle("slow",function(){
+		$(".hide").slideToggle("slow",function(){
 
 		});	
 	});
+
+	$(".show1").click(function(){
+
+		$(".hide1").slideToggle("slow",function(){
+			
+		});	
+	});
+
+	// language toggle 
+	$("#gb-flag").click(function(){
+		$("#swe-flag").toggle();
+		$("#gb-flag").hide();
+	});
+
+	$("#swe-flag").click(function(){
+		$("#gb-flag").toggle();
+		$("#swe-flag").hide();
+	});
+
+
 	//init. tooltip
 	$(function () {
 	  $('[data-toggle="tooltip"]').tooltip()
 	})
 });
+
+//JavaScript
+
+function toEnglish() {
+	document.getElementById("observe").innerHTML = "<strong>Sorry!</strong> This is only a Dummy-form. It doesn't work, yet. I appreciate you've come this far though!";
+  document.getElementById("name").innerHTML = "Name:";
+  document.getElementById("lastname").innerHTML = "Lastname:";
+  document.getElementById("question").innerHTML = "What's your question about?";
+  document.getElementById("questionForm").innerHTML = "Question:";
+  document.getElementById("send").innerHTML = "Send!";
+  document.getElementById("cancel").innerHTML = "Cancel";
+  alert("The language was changed to English");
+}
+
+function toSwedish() {
+	document.getElementById("observe").innerHTML = "<strong>Sorry!</strong> Detta är dessvärre bara en Dummy-formulär. Den fungerar riktigt ännu. Kul att du ändå kom så här långt!";
+  document.getElementById("name").innerHTML = "Förnamn:";
+  document.getElementById("lastname").innerHTML = "Efternamn:";
+  document.getElementById("question").innerHTML = "Vad handlar din fråga om?";
+  document.getElementById("questionForm").innerHTML = "Fråga:";
+  document.getElementById("send").innerHTML = "Skicka!";
+  document.getElementById("cancel").innerHTML = "Avbryt";
+  alert("Språket är ändrat till svenska");
+}
