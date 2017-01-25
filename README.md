f4a002618df99c9126b0a5d3dd1155ecca98a271
larrypalm <palm.gustav@gmail.com>
2017-01-25 23:30:39 +0100

    Moved all files from Weather-Search-repo to here, check repo for history

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-<h1>Slutprojekt Gustav Palm</h1>
+# Weather-Search
 
-This is the final projekt done by student Gustav Palm for the course "HTML, CSS, JavaScript och Webstandarder" at Lernia Yrkeshögskola. Part of the program "Javascriptutvecklare Front-End"
+<h1>Final task for "Webbapplicationsutveckling"-class by Gustav Palm</h1>
+
+This is the final task done by student Gustav Palm for the course "Webbapplicationsutveckling" at Lernia Yrkeshögskola. Part of the program "Javascriptutvecklare Front-End"
 
 <strong>The technologies used to complete this project are following:</strong>
 <ul>
 	<li>HTML5 & CSS: Used to create the backbone of the sites structure and not to forget some styling.</li>
 	<li>Bootstrap: I really like Bootsrap, that's why most of the styling and layout is managed by Bootstrap. The likes of the gridsystem, buttons, modals, lists are frequently used throughout the website using Bootstrap.</li>
-	<li>jQuery: Mainly used to create the drop-down animation of the corricolum section</li>
-	<li>JavaScript: Used to change the language of the </li>
+	<li>jQuery: Mainly used for layout changes and most inportantly used for AJAX-calls to web servers to use public API:s and access their JSON-data </li>
+	<li>JavaScript: Used to create and communicate with Google Maps Api</li>
 </ul>
 
-Please bare with me, I'm still learning alot as well as enjoying this alot! Hope you guys enjoy! 
+Hope you guys enjoy! 
 
 /Gustav
 
 Link to my LinkedIn profile: <a href="https://www.linkedin.com/in/gustav-palm-53393a114?trk=nav_responsive_tab_profile 
+">Gustav Palm</a></br>
+Link to the project live: <a href="https://www.linkedin.com/in/gustav-palm-53393a114?trk=nav_responsive_tab_profile 
 ">Gustav Palm</a>
--- js/main.js
+++ js/main.js
@@ -1,170 +1,388 @@
+var latitude;
+var longitude;
+	//initiering av google maps
+  function initMap(latitude, longitude) {
+
+    var markerPos = {lat: latitude, lng: longitude};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 8,
+      center: markerPos,
+      draggable: false,
+      zoomControl:false,
+      streetViewControl:false,
+      scrollwheel:false
+    });
+    
+    var marker = new google.maps.Marker({
+      position: markerPos,
+      map: map,
+
+    });
+  }
+
+  function initUvMap(latitude, longitude) {
+
+    var markerPos = {lat: latitude, lng: longitude};
+    var map = new google.maps.Map(document.getElementById('uvMap'), {
+      zoom: 4,
+      center: markerPos,
+      draggable: false,
+      zoomControl:false,
+      streetViewControl:false,
+      scrollwheel:false
+    });
+    
+    var marker = new google.maps.Marker({
+      position: markerPos,
+      setMap: uvMap,
+
+    });
+  }
+
+  //function för att sätta longitude och latitude
+  function retLatLng (latitude, longitude) {
+  	latitude = latitude;
+  	longitude = longitude
+  }
+
+
 $(document).ready(function(){
-
-  /*--------------------------------
-  	Skills
-  --------------------------------*/
-
-	$("#html, #css, #js").mouseenter(function(){
-		$(this).animate({
-			opacity:0.75,
-			
-		}, 200, function(){
-
-		});
-	});
-	$("#html, #css, #js").mouseleave(function(){
-		$(this).animate({
-				opacity:1
-			}, 200, function(){
-
-		});
+	$("#weatherSearch").hide();
+	$("#uvSearch").hide();
+	$("#forecastSearch").hide();
+	$("#ozoneSearch").hide();
+	$(".backToMenuBtn").hide();
+	$(".card").fadeIn("slow");
+	
+	//backToMenuBtn 
+	$(".backToMenuBtn").click(function(){
+		$("#weatherSearch").fadeOut("fast");
+		$("#uvSearch").fadeOut("fast");
+		$("#forecastSearch").fadeOut("fast");
+		$("#ozoneSearch").fadeOut("fast");
+		$("#weatherCard").fadeIn("slow");
+		$("#uvCard").fadeIn("slow");
+		$("#forecastCard").fadeIn("slow");
+		$("#ozoneCard").fadeIn("slow");	
+		$(".results").hide();	
 	});	
+	/**********************************
+	Vädret just nu
+	**********************************/	
+	$("#weatherBtn").click(function(){
+		$("#weatherSearch").toggle("slow");
+		$("#map").hide();
+		$("#weatherCard").hide();
+		$("#uvCard").hide();
+		$("#forecastCard").hide();
+		$("#ozoneCard").hide();
+		
+	});
+	/**********************************
+	/Vädret just nu
+	**********************************/
 
+	/**********************************
+	UV-index
+	**********************************/	
+	$("#uvBtn").click(function(){
+		$("#uvSearch").toggle("slow");
+		$("#weatherCard").hide();
+		$("#uvCard").hide();
+		$("#forecastCard").hide();
+		$("#ozoneCard").hide();
+		$(".backToMenuBtn").show();
 
-  /*--------------------------------
-  	Om
-  --------------------------------*/
-	$("#gustav").mouseenter(function(){
-		$(this).animate({
-			opacity:0.5,
+	});
+	/**********************************
+	/UV-index
+	**********************************/
+
+	/**********************************
+	Forecast-index
+	**********************************/	
+	$("#forecastBtn").click(function(){
+		$("#forecastSearch").toggle("slow");
+		$("#weatherCard").hide();
+		$("#uvCard").hide();
+		$("#forecastCard").hide();
+		$("#ozoneCard").hide();
+		$(".backToMenuBtn").show();
+
+	});
+	/**********************************
+	/Forecast-index
+	**********************************/
+
+	/**********************************
+	Ozone
+	**********************************/	
+	$("#ozoneBtn").click(function(){
+		$("#ozoneSearch").toggle("slow");
+		$("#weatherCard").hide();
+		$("#uvCard").hide();
+		$("#forecastCard").hide();
+		$("#ozoneCard").hide();
+		$(".backToMenuBtn").show();
+
+	});
+	/**********************************
+	/Ozone
+	**********************************/
+
+	/**********************************
+	Navbar Home
+	**********************************/
+	$("#navHome").click(function(){
+		$("#weatherSearch").fadeOut("fast");
+		$("#uvSearch").fadeOut("fast");
+		$("#forecastSearch").fadeOut("fast");
+		$("#ozoneSearch").fadeOut("fast");
+		$("#weatherCard").fadeIn("slow");
+		$("#uvCard").fadeIn("slow");
+		$("#forecastCard").fadeIn("slow");
+		$("#ozoneCard").fadeIn("slow");	
+		$(".results").hide();	
+	});
+	/**********************************
+	Navbar Vädret
+	**********************************/
+	$("#navWeather").click(function(){
+		$("#uvSearch").toggle("slow");
+		$("#weatherCard").hide();
+		$("#uvCard").hide();
+		$("#forecastCard").hide();
+		$("#ozoneCard").hide();
+		$(".backToMenuBtn").show();
+		
+	});
+
+	/**********************************
+	/Navbar Vädret
+	**********************************/	
+
+	/**********************************
+	Navbar uv
+	**********************************/
+	$("#navUv").click(function(){
+		$("#uvSearch").toggle("slow");
+		$("#map").hide();
+		$("#weatherCard").hide();
+		$("#uvCard").hide();
+		$("#forecastCard").hide();
+		$("#ozoneCard").hide();
+		
+	});
+
+	/**********************************
+	/Navbar üv
+	**********************************/	
+
+	/**********************************
+	Knappen trycks och JSON hämtas
+	**********************************/
+	$("#btn").click(function(){
+		$(".backToMenuBtn").show();
+		$("ul").fadeIn("fast");
+		$("#map").fadeIn("slow");
+		// Uppbyggnad av "Current Weather" api
+		var currentWeatherApi = "http://api.openweathermap.org/data/2.5/weather?q=";
+		var search = $("#searchBox").val();
+		var apiKey = "&APPID=ac2a79b6cd5460524fd1766d1c265114";
+		var unit = "&units=metric";
+		var url = currentWeatherApi + search + apiKey + unit;
+
+		$.getJSON(url , function(data){
+			//Hämtar JSON data ang. lat & lng för map
+			latitude = data.coord.lat;
+			longitude = data.coord.lon;
+			retLatLng(latitude,longitude);
+
+			//hämtar JSON data för aktuellt väder 
+			var temp = data.main.temp;
+			var id = data.weather[0].id;
+			var condition;
+			//hämtar JSON data för aktuellt väder 
+
+			if(id>=200 && id<300){
+				condition = "åskstorm"; 
+			}
+			else if (id>=300 && id<500){
+				condition ="duggregn";
+			} 
+			else if (id>=500 && id<600){
+				condition = "regn";
+			} 
+			else if (id>=600 && id<700){
+				condition ="snö";
+			} 
+
+			else if (id == 800){
+				condition ="snö";
+			}
+			else if (id == 741){
+				condition ="Fog";
+			}
+
+			$(".results").html("<li>"+temp+"°C"+" och "+condition+"</li>");
+			$("#headerCity").html("I "+search+" är vädret just nu"+":");
+      initMap(latitude,longitude);
+		});
+
+	});
+	/*
+	Knappen trycks och JSON hämtas
+	*/
+
+	/**********************************
+	UV knapp sökningen
+	**********************************/
+	$("#uvSearchBtn").click(function(){
+		// Uppbyggnad av "Current UV" api
+		$("ul").fadeIn("fast");
+		var geocodeApi = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+		var search = $("#uvSearchBox").val();
+		var geocodeApiKey = "&key=AIzaSyAuFutnl3HZQLaCVm4KwxBMd8xfPIfVwHg";
+		var geocodeUrl = geocodeApi+search+geocodeApiKey;
+		var uvIndex;
+		var colorCode;
+
+		$.getJSON(geocodeUrl , function(data){
+			latitude = data.results[0].geometry.location.lat;
+			longitude = data.results[0].geometry.location.lng;
 			
-		}, 500, function(){
+			var uvApi = "http://api.openweathermap.org/v3/uvi/";
+			var latLng = latitude.toFixed()+","+longitude.toFixed();
+			var timeStamp = "/current.json";
+			var uvApiKey = "?appid=ac2a79b6cd5460524fd1766d1c265114";
+			var uvUrl = uvApi + latLng + timeStamp + uvApiKey;			
 
+			$.getJSON(uvUrl , function(data){
+
+				var uv = data.data;
+				if(uv >= 0.0 && uv <= 2.9){
+					uvIndex = "Grön";
+					colorCode = "låg";
+				}
+				else if (uv>=3.0 && uv<=5.9){
+					uvIndex = "Yellow";
+					colorCode = "måttlig";					
+				}
+				else if (uv>=6.0 && uv<=7.9){
+					uvIndex = "Orange";
+					colorCode = "hög";					
+				}
+				else if (uv>=8.0 && uv<=10.9){
+					uvIndex = "Röd";
+					colorCode = "väldigt hög";					
+				}
+				else {
+					uvIndex = "Lila";
+					colorCode = "extrem";
+				}
+
+				$("#headerUv").html("I "+search+" är UV-indexet just nu :");
+				$(".results.results").html("<li>"+uv+"</li>");
+
+				$("#uvAnswer").html("UV-index "+"<strong>"+uv+"</strong>"+ " innebär färgkod "+"<strong>"+uvIndex+"</strong>"+ " som i sin tur innebär att risken för skada vid exponering mot solen utan solkräm för den genomsnittlige människan är " + "<strong>"+colorCode+"</strong>");
+
+			});
 		});
 	});
-	$("#gustav").mouseleave(function(){
-		$(this).animate({
-			opacity:1
-		}, 1000, function(){
+	/**********************************
+	/UV knapp sökningen
+	**********************************/
 
-		});
-	});	
+	/**********************************
+	forecast knapp sökningen
+	**********************************/
+	$("#forecastSearchBtn").click(function(){
+		var forecastApi = "http://api.openweathermap.org/data/2.5/forecast?q=";
+		var search = $("#forecastSearchBox").val();
+		var modeJson = "&mode=json";
+		var forecastApiKey = "&appid=ac2a79b6cd5460524fd1766d1c265114";
+		var unit = "&units=metric";
+		var forecastUrl = forecastApi + search + modeJson + forecastApiKey +unit;			
+		console.log(forecastUrl);
 
-  /*--------------------------------
-  	CV
-  --------------------------------*/
-	//cascade CV	
-	$(".show").click(function(){
-
-		$(".hide").slideToggle("slow",function(){
-
-		});	
-	});
-
-	$(".show1").click(function(){
-		$(".hide1").slideToggle("slow",function(){
+		$.getJSON(forecastUrl , function(data){
+			//latitude = data.results[0].geometry.location.lat;
+			//longitude = data.results[0].geometry.location.lng;
 			
-		});	
-	});
+			var hour1 = data.list[0].main.temp;
+			var timeStamp1 = data.list[0].dt_txt;
+			var hour2 = data.list[1].main.temp;
+			var timeStamp2 = data.list[1].dt_txt;
+			var hour3 = data.list[2].main.temp;
+			var timeStamp3 = data.list[2].dt_txt;
+			var hour4 = data.list[3].main.temp;
+			var timeStamp4 = data.list[3].dt_txt;
+			var hour5 = data.list[4].main.temp;
+			var timeStamp5 = data.list[4].dt_txt;
 
-	// language toggle 
-	$("#gb-flag").click(function(){
-		$("#swe-flag").toggle();
-		$("#gb-flag").hide();
-	});
+			$(".results").html("<li><strong>Datum/tid</strong>: "+timeStamp1+" <strong>Temperatur: </strong>"+hour1+"</li></br>");
 
-	$("#swe-flag").click(function(){
-		$("#gb-flag").toggle();
-		$("#swe-flag").hide();
-	});
+			$("#headerForecast").html("De närmsta 12h i "+search+":");
 
-	//init. tooltip
-	$(function () {
-	  $('[data-toggle="tooltip"]').tooltip()
-	})
+			$(".results").append("<li><strong>Datum/tid</strong>: " +timeStamp2+ " <strong>Temperatur: </strong>"+hour2+"°C</li></br>");
+			$(".results").append("<li><strong>Datum/tid</strong>: " +timeStamp3+ " <strong>Temperatur: </strong>"+hour3+"°C</li></br>");
+			$(".results").append("<li><strong>Datum/tid</strong>: " +timeStamp4+ " <strong>Temperatur: </strong>"+hour4+"°C</li></br>");
+			$(".results").append("<li><strong>Datum/tid</strong>: " +timeStamp5+ " <strong>Temperatur: </strong>"+hour5+"°C</li></br>");
 
-  /*--------------------------------
-  	Social media
-  --------------------------------*/
+			/*$.getJSON(uvUrl , function(data){
+				console.log(uvUrl);
 
-  $("#fb, #github, #linkedin").css("text-decoration", "none");
+				var uv = data.data;
+				console.log(uv);
 
-});	
-
-//JavaScript
-function changeRed() {
-	document.getElementById("gustavpalm").style.color = "red";
-} 
-
-function toEnglish() {
-	document.getElementById("qBtn").innerHTML = "Ask a question!";
-
-	document.getElementById("observe").innerHTML = "<strong>Sorry!</strong> This is only a Dummy-form. It doesn't work, yet. I appreciate you've come this far though!";
-	document.getElementById("name").innerHTML = "Name:";
-  document.getElementById("lastname").innerHTML = "Lastname:";
-  document.getElementById("question").innerHTML = "What's your question about?";
-  document.getElementById("questionForm").innerHTML = "Question:";
-  document.getElementById("send").innerHTML = "Send!";
-  document.getElementById("cancel").innerHTML = "Cancel";
-	swal({
-		title: "The language is set to English",
-	  confirmButtonColor: "#428bca",
-	  confirmButtonText: "Close",
-		closeOnConfirm: true
-	});
-}
-
-function toSwedish() {
-	document.getElementById("qBtn").innerHTML = "Ställ en fråga!";
-
-	document.getElementById("observe").innerHTML = "<strong>Sorry!</strong> Detta är dessvärre bara en Dummy-formulär. Den fungerar riktigt ännu. Kul att du ändå kom så här långt!";
-  document.getElementById("name").innerHTML = "Förnamn:";
-  document.getElementById("lastname").innerHTML = "Efternamn:";
-  document.getElementById("question").innerHTML = "Vad handlar din fråga om?";
-  document.getElementById("questionForm").innerHTML = "Fråga:";
-  document.getElementById("send").innerHTML = "Skicka!";
-  document.getElementById("cancel").innerHTML = "Avbryt";
-	swal({
-		title: "Språket har ändrats till Svenska",
-	  confirmButtonColor: "#428bca",
-	  confirmButtonText: "Stäng",
-		closeOnConfirm: true
-	});
-}
-
-function send(){
-	$("#send").on("click", function(){
-		swal({
-		  title: "Tack för din fråga!",
-		  text: "Jag ska göra mitt yttersta för att svara på den",
-		  type: "success",
-		  confirmButtonColor: "#428bca",
-		  confirmButtonText: "Stäng",
-		  closeOnConfirm: true
+				if(uv >= 0.0 && uv <= 2.9){
+					uvIndex = "Green";
+				}
+				$("#headerUv").html("I "+search+" är UV-indexet just nu"+":");
+				$("ul").html("<li>"+uv+"</li>");
+				initUvMap(latitude,longitude);	
+			});*/
 		});
+	/**********************************
+	/forecast knapp sökningen
+	**********************************/
 	});
-}
 
-function cancel(){
-	$("#cancel").on("click", function(){
-		swal({
-		  title: "Är du säker?",
-		  type: "warning",
-		  showCancelButton: true,
-		  confirmButtonColor: "#428bca",
-		  confirmButtonText: "Ja",
-		  cancelButtonText: "Avbryt",
-		  closeOnConfirm: false,
-		  closeOnCancel: false
-		},
-		function(isConfirm){
-		  if (isConfirm) {
-				swal({
-				  title: "Ditt meddelande är raderat!",
-				  type: "success",
-				  confirmButtonColor: "#428bca",
-				  confirmButtonText: "Stäng",
-				  closeOnConfirm: true
+	/**********************************
+	Ozone
+	**********************************/	
+		$("#ozoneSearchBtn").click(function(){
+			var geocodeApi = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+			var search = $("#ozoneSearchBox").val();
+			var geocodeApiKey = "&key=AIzaSyAuFutnl3HZQLaCVm4KwxBMd8xfPIfVwHg";
+			var geocodeUrl = geocodeApi+search+geocodeApiKey;
+			$.getJSON(geocodeUrl , function(data){
+
+				latitude = data.results[0].geometry.location.lat;
+				longitude = data.results[0].geometry.location.lng;
+				var ozoneApi = "http://api.openweathermap.org/pollution/v1/o3/";
+				var latLng = latitude.toFixed() + "," + longitude.toFixed();
+				var timeStamp = "/current.json";
+				var ozoneApiKey = "?appid=ac2a79b6cd5460524fd1766d1c265114";
+				var ozoneUrl = ozoneApi + latLng + timeStamp + ozoneApiKey;
+
+				$.getJSON(ozoneUrl, function(data){
+					var ozoneIndex = data.data;
+					var ozoneMetric = ozoneIndex/100;
+					$("#headerOzone").html("Ozonlagret över "+search+" är :");
+					$(".results").html("<li>"+ozoneMetric+" milimeter tjockt</li>");						
 				});
-		  } else {
-				swal({
-				  title: "Är du säker?",
-				  text: "You will not be able to recover this imaginary file!",
-				  confirmButtonColor: "#428bca",
-				  confirmButtonText: "Stäng",
-				  closeOnConfirm: true
-				});
-		  }
+			});
 		});
-	});
-}
+
+
+
+	/**********************************
+	/Ozone
+	**********************************/	
+	//http://api.openweathermap.org/pollution/v1/o3/0.0,10.0/current.json?appid=ac2a79b6cd5460524fd1766d1c265114
+
+}); /*document ready*/
+
+
+
